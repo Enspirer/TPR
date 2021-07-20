6b78b2135b159b4d2332f663c468dd48357935d6
imzajjithvedha <zajjith@gmail.com>
2021-07-20 23:30:40 +0530

    map script fixed

--- resources/views/frontend/layouts/theme_app.blade.php
+++ resources/views/frontend/layouts/theme_app.blade.php
@@ -99,13 +99,14 @@
 
 <!-- Scripts -->
 
-<!-- <script src="scripts/map.js"></script> -->
+
 @stack('before-scripts')
+<script src="{{ asset('tpr_templete/scripts/map.js') }}"></script>
 <script src="{{ asset('tpr_templete/scripts/main.js') }}"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<!-- <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyArF7tuecnSc3AvTh5V_mabinQqE6TuiYM&callback=initMap"
-type="text/javascript"></script> -->
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyArF7tuecnSc3AvTh5V_mabinQqE6TuiYM&callback=initMap"
+type="text/javascript"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script>
     AOS.init();
