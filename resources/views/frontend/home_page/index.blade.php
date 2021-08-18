6eeae916b8a2a246e577b56c89be9fe185e28e92
sanjayaharshana <sanjayaharasana@gmail.com>
2021-08-18 17:15:58 +0530

    Index Blades

--- resources/views/frontend/home_page/index.blade.php
+++ resources/views/frontend/home_page/index.blade.php
@@ -430,7 +430,8 @@
 
                 $.post("{{url('/')}}/api/country_request",
                     {
-                        coordinate_data: myArray
+                        coordinate_data: myArray,
+                        country_id : 'LK'
                     },
                     function(data, status){
 
@@ -460,7 +461,7 @@
                                                         <h5 class="fw-bold mb-2">${obj[i]['name']}</h5>
                                                         <p class="mb-1" style="font-size: 0.8rem;">Transaction Type: ${obj[i]['transaction_type']}</p>
                                                         <p class="mb-1" style="font-size: 0.8rem;">Country: ${obj[i]['country']}</p>
-                                                        <p class="mb-0 d-inline-block px-2 py-1 mt-2 text-light" style="font-size: 0.8rem; background: #4195e1; border-radius: 7px;">Price : ${obj[i]['price']}</p>
+                                                        <p class="mb-0 d-inline-block px-2 py-1 mt-2 text-light" style="font-size: 0.8rem; background: #4195e1; border-radius: 7px;">Price : ${obj[i]['price_currency']}</p>
 
                                                         <div class="text-end">
                                                             <a href="{{url('/')}}/individual-property/${obj[i]['id']}" class="btn px-3 rounded-0 text-light py-1" style="background-color: #4195E1">VIEW</a>
@@ -495,7 +496,7 @@
                                         <p class="fw-bold mb-0">${obj[i]['name']}</p>
                                         <p class="mb-0" style="font-size: 0.8rem;">Transaction Type: ${obj[i]['transaction_type']}</p>
                                         <p class="mb-0" style="font-size: 0.8rem;">Country: ${obj[i]['country']}</p>
-                                        <p class="mb-0 d-inline-block px-2 py-1 mt-2 text-light mb-1" style="font-size: 0.8rem; background: #4195e1; border-radius: 7px;">Price : ${obj[i]['price']}</p>
+                                        <p class="mb-0 d-inline-block px-2 py-1 mt-2 text-light mb-1" style="font-size: 0.8rem; background: #4195e1; border-radius: 7px;">Price : ${obj[i]['price_currency']}</p>
                                     </div>
                                 </div>
                             `
