df01bf3c3822976981d8642be8ee74139f16acd9
imzajjithvedha <zajjith@gmail.com>
2021-08-24 17:29:17 +0530

    prices country

--- resources/views/frontend/user/properties.blade.php
+++ resources/views/frontend/user/properties.blade.php
@@ -89,7 +89,12 @@
                                                     <p class="card-text mt-3 mb-1">{{ $property['beds'] }} Bed Semidetached house</p>
                                                 @endif                                            
                                             <p class="card-text">Property Type : {{ App\Models\PropertyType::where('id', $property->property_type)->first()->property_type_name }}</p>
-                                            <p class="mt-1 text-info">$ {{ $property['price'] }}</p>
+                                            <!-- <p class="mt-1 text-info">$ {{ $property['price'] }}</p> -->
+                                            @if(get_country_cookie(request()))
+                                                <p class="mb-0 d-inline-block px-2 py-1 text-light mb-3" style="font-size: 0.8rem; background: #4195e1; border-radius: 7px;">{{ current_price(get_country_cookie(request())->country_id, $property->price) }}</p>
+                                            @else
+                                                <p class="mb-0 d-inline-block px-2 py-1 text-light mb-3" style="font-size: 0.8rem; background: #4195e1; border-radius: 7px;">{{ current_price(1, $property->price) }}</p>
+                                            @endif
 
                                             <div class="row">
                                                 <div class="col-9">
