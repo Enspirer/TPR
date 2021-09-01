427ff382d48b42ca1f99192fa96cf3787cce4a09
nihsaan7 <nihsaan.enspirer@gmail.com>
2021-09-01 19:13:42 +0530

    backend modal close button,settings edit button

--- app/Http/Controllers/Backend/SettingsController.php
+++ app/Http/Controllers/Backend/SettingsController.php
@@ -25,8 +25,8 @@ class SettingsController extends Controller
             
                     ->addColumn('action', function($data){
                        
-                        $button = '<a href="'.route('admin.settings.edit',$data->id).'" name="edit" id="'.$data->id.'" class="edit btn btn-secondary btn-sm ml-3" style="margin-right: 10px"><i class="fas fa-edit"></i> Edit </a>';
-                        $button .= '<button type="button" name="delete" id="'.$data->id.'" class="delete btn btn-danger btn-sm"><i class="fas fa-trash"></i> Delete</button>';
+                        // $button = '<a href="'.route('admin.settings.edit',$data->id).'" name="edit" id="'.$data->id.'" class="edit btn btn-secondary btn-sm ml-3" style="margin-right: 10px"><i class="fas fa-edit"></i> Edit </a>';
+                        $button = '<button type="button" name="delete" id="'.$data->id.'" class="delete btn btn-danger btn-sm"><i class="fas fa-trash"></i> Delete</button>';
                         return $button;
                     })
 
