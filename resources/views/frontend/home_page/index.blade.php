f526ce8614040330fc84d90af04780f40f233098
Zajjith Vedha <zajjith@gmail.com>
2021-10-06 13:48:44 +0530

    country zoom

--- resources/views/frontend/home_page/index.blade.php
+++ resources/views/frontend/home_page/index.blade.php
@@ -433,7 +433,7 @@
 
         function initMap() {
             const map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 7,
+                zoom: 5,
                 center: { lat: parseInt(lat), lng: parseInt(lng) },
             });
 
