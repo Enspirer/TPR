2f565e983f4bf48f67ebaf6bd07b51802187643b
nihsaan7 <nihsaan.enspirer@gmail.com>
2022-01-06 19:04:10 +0530

    watermark

--- app/Http/Controllers/Frontend/FileManagerController.php
+++ app/Http/Controllers/Frontend/FileManagerController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\FileManager;
 use Illuminate\Http\Request;
 use File;
+use Image;
 
 use DataTables;
 
@@ -20,7 +21,14 @@ class FileManagerController extends Controller
     {
         $image = $request->file('file');
         $imageName = time().'_'.rand(1000,10000).'.'.$image->getClientOriginalExtension();
-        $image->move(public_path('images'),$imageName);
+
+        $imgFile = Image::make($image->getRealPath());
+
+        $imgFile->insert('tropical_logo.png')->save(public_path('/images').'/'.$imageName); 
+
+        // $image->move(public_path('images'),$imageName);
+
+
         $fileManager = new FileManager;
         $fileManager->file_name = $imageName;
         $fileManager->user_id = auth()->user()->id;
