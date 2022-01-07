409f91eb5f7c7ec428ea22cccb7de615780d5f26
prasadbanuka <banuka.mawelage@gmail.com>
2022-01-07 19:30:05 +0530

    favorite

--- resources/views/frontend/user/favourites.blade.php
+++ resources/views/frontend/user/favourites.blade.php
@@ -10,7 +10,7 @@
 
     <div class="container user-settings" style="margin-top:8rem; margin-bottom: 5rem;">
         <div class="row justify-content-between">
-            <div class="col-4">
+            <div class="col-4 full-size-width">
                 <div class="row">
                     <div class="col-12">
                         @include('frontend.includes.profile-settings-links')
@@ -18,7 +18,7 @@
                 </div>
             </div>
 
-            <div class="col-8">
+            <div class="col-8 full-size-width">
 
                 <div class="row justify-content-between">
                     <div class="col-8 p-0">
