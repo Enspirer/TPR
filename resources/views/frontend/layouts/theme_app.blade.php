de15e73bd8ee7f93a9b298a00ffbf9c8833c8729
prasadbanuka <banuka.mawelage@gmail.com>
2022-01-08 13:33:08 +0530

    ad popup button

--- resources/views/frontend/layouts/theme_app.blade.php
+++ resources/views/frontend/layouts/theme_app.blade.php
@@ -578,7 +578,17 @@ padding: 0;
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary" onclick="window.location.href='{{route('frontend.auth.login')}}'">Become a Agent</button>
+
+        @auth
+
+        @if(App\Models\AgentRequest::where('user_id',auth()->user()->id)->first() == null)
+        <button type="button" class="btn btn-primary" onclick="window.location.href='{{route('frontend.user.agent')}}'">Become a Agent</button>
+        @else
+        <button type="button" class="btn btn-primary" onclick="window.location.href='{{route('frontend.user.properties')}}'">Add Property</button>
+        @endif
+        @else 
+        <button type="button" class="btn btn-primary" onclick="window.location.href='{{route('frontend.auth.register')}}'">Create Account</button>
+        @endauth
       </div>
     </div>
   </div>
