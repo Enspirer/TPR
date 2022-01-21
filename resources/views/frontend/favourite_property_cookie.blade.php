0c263fdb52135fbc67895f5f48c9b2db809c75ef
nihsaan7 <nihsaan.enspirer@gmail.com>
2022-01-21 15:13:56 +0530

    changes

--- resources/views/frontend/favourite_property_cookie.blade.php
+++ resources/views/frontend/favourite_property_cookie.blade.php
@@ -15,17 +15,18 @@
             <h3 class="fw-bolder">Property Favourite</h3>
 
             <div class="row mt-4 mb-5" style="text-align:justify;">
-                <div class="col-10">
+                <div class="col-12">
                     
                     @if(count($favorite_item) != 0)
                         @foreach($favorite_item as $prop)  
                             <div class="row align-items-center justify-content-between mb-4 border py-1">
-                                <div class="col-6">
-                                    <a href="{{ route('frontend.individual-property', $prop->id) }}"><img src="{{url('image_assest', App\Models\Properties::where('id',$prop->id)->first()->feature_image_id)}}" style="width:350px; object-fit:cover;" height="160px" class="card-img-top" alt="..."></a>
+                                <div class="col-4">
+                                    <a href="{{ route('frontend.individual-property', $prop->id) }}"><img src="{{url('image_assest', App\Models\Properties::where('id',$prop->id)->first()->feature_image_id)}}" style="width:350px; object-fit:cover;" height="180px" class="card-img-top" alt="..."></a>
                                 </div>
-                                <div class="col-6">
+                                <div class="col-8">
                                     <h5 class="card-title">{{ $prop->name }}</h5>
-                                    <h5 class="card-title" style="font-size:13px; font-family:calibri; overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical;">{{App\Models\Properties::where('id',$prop->id)->first()->description}}</h5>
+                                    <h5 class="card-title" style="font-size:15px; font-family:calibri; overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical;">{{App\Models\Properties::where('id',$prop->id)->first()->description}}</h5>
+                                    <h5 class="card-title" style="font-size:14px;">Transaction Type: {{App\Models\Properties::where('id',$prop->id)->first()->transaction_type}}</h5>                                    
                                     @if(get_country_cookie(request()))
                                         <p class="mt-1 text-primary">{{ current_price(request(), get_country_cookie(request())->country_id, $prop->price) }}</p>      
                                     @else                              
@@ -33,7 +34,7 @@
                                     @endif                               
                                     <div class="row">
                                         <div class="col-4">
-                                            <a href="{{ route('frontend.individual-property', $prop->id) }}" class="btn px-4 rounded-0 text-light py-1" style="background-color: #4195E1">View</a>
+                                            <a href="{{ route('frontend.individual-property', $prop->id) }}" class="btn px-4 rounded-0 text-light" style="background-color: #4195E1">View</a>
                                         </div>
                                     </div>
                                 </div>
